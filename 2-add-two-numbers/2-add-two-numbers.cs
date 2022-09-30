c7e79de517e1ce5a447d48260e900de6d2cac631
Supansa Tantulset <69199037+SupansaTan@users.noreply.github.com>
2022-09-30 20:01:39 +0700

    Time: 100 ms (92.34%), Space: 49.5 MB (6.03%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cs
+++ 2-add-two-numbers/2-add-two-numbers.cs
@@ -15,15 +15,17 @@ public class Solution {
         ListNode currentNode = new ListNode();
         ListNode resultNode = currentNode;
         
-        while (l1 != null || l2 != null || carry > 0)
+        while (l1 is not null || l2 is not null || carry > 0)
         {
-            int sum = (l1 != null ? l1.val : 0) + (l2 != null ? l2.val : 0) + carry;
+            int sum = (l1 is not null ? l1.val : 0) 
+                    + (l2 is not null ? l2.val : 0)
+                    + carry;
             carry = (int) sum / 10;
             currentNode.next = new ListNode(sum % 10);
             currentNode = currentNode.next;
             
-            l1 = l1 != null ? l1.next : null;
-            l2 = l2 != null ? l2.next : null;
+            l1 = l1 is not null ? l1.next : null;
+            l2 = l2 is not null ? l2.next : null;
         }
         return resultNode.next;
     }
