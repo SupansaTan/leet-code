40dce929df8ef1653818895903a603d6865eb158
Supansa Tantulset <69199037+SupansaTan@users.noreply.github.com>
2022-10-04 15:10:19 +0700

    Time: 145 ms (28.85%), Space: 37.8 MB (80.35%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cs
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cs
@@ -1,21 +1,8 @@
 public class Solution {
     public int FirstUniqChar(string s) {
-        int[] count = new int[26];
-        int length = s.Length;
-        
-        // count char --> O(N)
-        for (int i = 0; i < length; i++)
+        for (int i = 0; i < s.Length; i++)
         {
-            int index = (int)(s[i]- 'a');
-            count[index]++;
-        }
-        
-        // find index --> O(26)
-        for (int i = 0; i < length; i++)
-        {
-            int index = (int)(s[i]- 'a');
-            if (count[index] == 1)
-            {
+            if (s.IndexOf(s[i], s.IndexOf(s[i]) + 1) == -1) {
                 return i;
             }
         }
