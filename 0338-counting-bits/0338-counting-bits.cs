db428d6b982eba384152d007b17913c975d1f4ae
Supansa Tantulset <69199037+SupansaTan@users.noreply.github.com>
2022-10-20 02:26:44 +0700

    Time: 167 ms (53.55%), Space: 38.5 MB (35.65%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cs
+++ 0338-counting-bits/0338-counting-bits.cs
@@ -1,3 +1,21 @@
+/*
+    n:   0 1 2 3 4 5 6 7 8
+    out: 0 1 1 2 1 2 2 3 1
+    
+    - power 1 -
+    out[2] = out[0] + 1 => out[2-2] + 1
+    out[3] = out[1] + 1 => out[3-2] + 1
+    
+    - power 2 -
+    out[4] = out[0] + 1 => out[4-4] + 1
+    out[5] = out[1] + 1 => out[5-4] + 1
+    out[6] = out[2] + 1 => out[6-4] + 1
+    out[7] = out[3] + 1 => out[7-4] + 1
+    
+    - power 3 -
+    out[8] = out[0] + 1 => out[8-8] + 1
+*/
+
 public class Solution {
     public int[] CountBits(int n) {
         // base case
