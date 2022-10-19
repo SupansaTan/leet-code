6c3a30e7c89527c37118c8d64a722a99e8567037
Supansa Tantulset <69199037+SupansaTan@users.noreply.github.com>
2022-10-20 02:06:16 +0700

    Time: 171 ms (47.30%), Space: 37.8 MB (98.72%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.cs
@@ -0,0 +1,40 @@
+/*
+    n:   0 1 2 3 4 5 6 7 8
+    out: 0 1 1 2 1 2 2 3 1
+    
+    - power 1 -
+    out[2] = out[0] + 1 => out[2-2] + 1
+    out[3] = out[1] + 1 => out[3-2] + 1
+    
+    - power 2 -
+    out[4] = out[0] + 1 => out[4-4] + 1
+    out[5] = out[1] + 1 => out[5-4] + 1
+    out[6] = out[2] + 1 => out[6-4] + 1
+    out[7] = out[3] + 1 => out[7-4] + 1
+    
+    - power 3 -
+    out[8] = out[0] + 1 => out[8-8] + 1
+*/
+
+public class Solution {
+    public int[] CountBits(int n) {
+        int[] count = new int[n+1];
+        count[0] = 0;
+        
+        if (n < 1)
+        {
+            return count;
+        }
+
+        int pow = 1;
+        for (int i = 1; i < n+1; i++)
+        {
+            if (i == pow * 2)
+            {
+                pow *= 2;
+            }
+            count[i] = count[i-pow] + 1;
+        }
+        return count;
+    }
+}
