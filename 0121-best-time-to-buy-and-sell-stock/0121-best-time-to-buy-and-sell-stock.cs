0be528e3a726200f6b12f4427c987180c2b0aa26
Supansa Tantulset <69199037+SupansaTan@users.noreply.github.com>
2022-10-28 16:21:08 +0700

    Time: 514 ms (18.59%), Space: 45.8 MB (99.07%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cs
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cs
@@ -4,16 +4,15 @@
 
 public class Solution {
     public int MaxProfit(int[] prices) {
-        int dayLength = prices.Length;
-        int maxProfit = 0;
+        int profit, maxProfit = 0;
         int left = 0;
         int right = 1;
         
-        while (right < dayLength)
+        while (right < prices.Length)
         {
             if (prices[left] < prices[right])
             {
-                int profit = prices[right] - prices[left];
+                profit = prices[right] - prices[left];
                 maxProfit = Math.Max(maxProfit, profit);
             }
             else
